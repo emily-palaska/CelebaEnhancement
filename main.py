89cfffcc88d4f5d0dee3fdf5a554e41e0fde2689
Emily Palaska <aimilia.p2@gmail.com>
2025-01-09 18:55:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from source.trainer import Trainer
 
 def main():
-    # params = {'backbone': 'default',
-    #          'g_lr': 1e-3,
-    #          'd_lr': 1e-4,}
-    params = {'lr':0.001}
-    trainer = Trainer(net_type='rbf',
+    params = {'backbone': 'default',
+              'g_lr': 1e-3,
+              'd_lr': 1e-4,}
+    #params = {'lr':0.001}
+    trainer = Trainer(net_type='gan',
                       num_samples=20,
                       num_epochs=1,
                       batch_size=2,
